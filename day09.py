4c6d9d9ae85983cf211c0f535b3fad77ecc1eb59
Davvos11 <vosdavid2@gmail.com>
2022-12-10 18:38:12 +0100

    day 9 part 2

--- day09.py
+++ day09.py
@@ -1,9 +1,9 @@
 def main(filename: str):
-    head = [0, 0]
-    tail = [0, 0]
+    # List of nots (0 = H)
+    knots = [[0, 0] for i in range(0, 10)]
     visited = set()
 
-    print(f"\t\t Head: {head}, Tail: {tail}")
+    print(f"\t\t {knots}")
 
     with open(filename) as file:
         for line in file:
@@ -11,49 +11,60 @@ def main(filename: str):
             direction = line[0]
             amount = int(line[2:])
 
-            for i in range(amount):
+            for _ in range(amount):
                 # Move head
                 if direction == "R":
-                    head[1] += 1
+                    knots[0][1] += 1
                 elif direction == "L":
-                    head[1] -= 1
+                    knots[0][1] -= 1
                 elif direction == "U":
-                    head[0] += 1
+                    knots[0][0] += 1
                 elif direction == "D":
-                    head[0] -= 1
+                    knots[0][0] -= 1
 
-                # Move tail based on position of head
-                if head[0] == tail[0]:  # If on same row
-                    if abs(head[1] - tail[1]) > 1:
-                        if head[1] > tail[1]:
-                            tail[1] += 1
-                        elif head[1] < tail[1]:
-                            tail[1] -= 1
-                elif head[1] == tail[1]:  # If on same column
-                    if abs(head[0] - tail[0]) > 1:
-                        if head[0] > tail[0]:
-                            tail[0] += 1
-                        elif head[0] < tail[0]:
-                            tail[0] -= 1
-                else:  # If diagonally
-                    if abs(head[0] - tail[0]) <= 1 and abs(head[1] - tail[1]) <= 1:
-                        pass
-                    else:
-                        if head[0] > tail[0]:
-                            tail[0] += 1
-                        elif head[0] < tail[0]:
-                            tail[0] -= 1
-                        if head[1] > tail[1]:
-                            tail[1] += 1
-                        elif head[1] < tail[1]:
-                            tail[1] -= 1
+                for i in range(len(knots)-1):
+                    # Head is the knot in front of the knot we are moving
+                    head = knots[i]
+                    # Tail is the knot that we are moving now
+                    tail = knots[i+1]
+                    move_tail(head, tail)
 
-                print(f"{line}:\t Head: {head}, Tail: {tail}")
+                print(f"{line}:\t {knots}")
                 # Save tail location
-                visited.add(tuple(tail))
+                visited.add(tuple(knots[-1]))
 
     print(visited)
     print(len(visited))
+
+
+def move_tail(head: [int], tail: [int]) -> ([int], [int]):
+    # Move tail based on position of head
+    if head[0] == tail[0]:  # If on same row
+        if abs(head[1] - tail[1]) > 1:
+            if head[1] > tail[1]:
+                tail[1] += 1
+            elif head[1] < tail[1]:
+                tail[1] -= 1
+    elif head[1] == tail[1]:  # If on same column
+        if abs(head[0] - tail[0]) > 1:
+            if head[0] > tail[0]:
+                tail[0] += 1
+            elif head[0] < tail[0]:
+                tail[0] -= 1
+    else:  # If diagonally
+        if abs(head[0] - tail[0]) <= 1 and abs(head[1] - tail[1]) <= 1:
+            pass
+        else:
+            if head[0] > tail[0]:
+                tail[0] += 1
+            elif head[0] < tail[0]:
+                tail[0] -= 1
+            if head[1] > tail[1]:
+                tail[1] += 1
+            elif head[1] < tail[1]:
+                tail[1] -= 1
+
+    return head, tail
 
 
 if __name__ == '__main__':
