b7d8b470e877f13299e0c5fb125c95032dd1f08a
Davvos11 <vosdavid2@gmail.com>
2022-12-14 00:28:28 +0100

    day 12 part 2

--- day12.py
+++ day12.py
@@ -1,7 +1,8 @@
-def main(filename: str):
+def parse(filename: str):
     heightmap = []
     start = (0, 0)
     end = (0, 0)
+    zeroes = []
 
     with open(filename) as file:
         for i, line in enumerate(file):
@@ -15,8 +16,14 @@ def main(filename: str):
                     end = (i, j)
                     value = 25
                 row.append(value)
+                if value == 0:
+                    zeroes.append((i, j))
             heightmap.append(row)
 
+    return heightmap, start, end, zeroes
+
+
+def bfs(heightmap: [[int]], start: (int, int), end: (int, int)):
     queue = []
     visited = set()
     lengths = dict()
@@ -31,8 +38,7 @@ def main(filename: str):
         current_length = lengths[location]
 
         if location == end:
-            print(current_length)
-            return
+            return current_length
 
         for offset in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
             # Get next location
@@ -55,5 +61,17 @@ def main(filename: str):
                 lengths[next_location] = current_length + 1
 
 
+def main():
+    heightmap, start, end, zeroes = parse("input/day12.txt")
+    print(f"Part one: {bfs(heightmap, start, end)}")
+
+    shortest = None
+    for start_option in zeroes:
+        length = bfs(heightmap, start_option, end)
+        if shortest is None or length is not None and length < shortest:
+            shortest = length
+    print(f"Part two: {shortest}")
+
+
 if __name__ == '__main__':
-    main("input/day12.txt")
+    main()
